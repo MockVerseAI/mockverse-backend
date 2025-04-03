000d010ecefe4a398d807a6611bd26321088ff53
Ayush Kumar <ayushsaha111@gmail.com>
2025-04-03 19:39:37 +0530

    fix: security bugs (#37)
    * fix: security bugs
    
    * feat: xss sanitize

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -80,7 +80,7 @@ userSchema.methods.generateAccessToken = function () {
       role: this.role,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY, algorithm: "HS256" }
   );
 };
 
@@ -90,7 +90,7 @@ userSchema.methods.generateRefreshToken = function () {
       _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY, algorithm: "HS256" }
   );
 };
 
