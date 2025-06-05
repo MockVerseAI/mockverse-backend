69e47a47ff6a12d756a4b99b38bf60f51949f8fc
ayushsaha1018 <ayushsaha111@gmail.com>
2025-06-05 09:41:47 +0530

    fix: redis

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,7 +1,10 @@
+import { isRedisAvailable } from "../config/redis.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
+  await isRedisAvailable();
+
   return res
     .status(200)
     .json(new ApiResponse(200, "OK", "Health check passed"));
